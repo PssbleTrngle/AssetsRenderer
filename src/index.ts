3c454bfe2d810ca8e55c706633f5ed67dd88e88a
PssbleTrngle <nik@somethingcatchy.net>
2022-12-08 00:11:57 +0100

    index typings

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as RenderOptions } from './cli/options.js'
+export { default as render, renderUsing } from './renderer/index.js'
+export * from './renderer/math.js'
+export { default as ModelRenderer } from './renderer/ModelRenderer.js'
+export * from './renderer/models.js'
